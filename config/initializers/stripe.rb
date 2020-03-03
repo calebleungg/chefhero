ec17d16f4204637f9140ce4c101684da4fc437df
Caleb Leung <caleb.leungg@gmail.com>
2020-03-03 14:18:53 +1000

    adjusting initializer to fix above?

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -5,4 +5,6 @@
 #   :secret_key => Rails.application.credentials.dig(:stripe, :secret_key)    
 # }    
 
-Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+# Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+
+Stripe.api_key = Rails.application.credentials.stripe[:secret_key]
